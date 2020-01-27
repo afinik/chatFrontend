9458046de12858a1f49a99cc29b6778b34061659
smilex <alexdate@gmail.com>
2020-01-27 17:15:39 +0300

    Версия для чата

--- app/src/main/java/com/example/testchat/MainActivity.java
+++ app/src/main/java/com/example/testchat/MainActivity.java
@@ -1,98 +1,86 @@
 package com.example.testchat;
 
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.NetworkOnMainThreadException;
 import android.widget.TextView;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
-import java.io.FileNotFoundException;
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.io.PrintWriter;
 import java.net.Socket;
-import java.net.UnknownHostException;
-import java.util.concurrent.atomic.AtomicInteger;
-
 
 public class MainActivity extends Activity {
 
+    Connect connect = new Connect();
+    Thread thread = new Thread();
 
-
-    Chat chat = new Chat();
-
+    @SuppressLint("WrongThread")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         TextView info = (TextView) findViewById(R.id.info1);
-        chat.run();
+        connect.doInBackground(new String[1]);
         info.setText("Запускаем процесс");
+
+
     }
-
-//    MainActivity(String nick) {
-//        this.nick = nick;
-//    }
-
-
-
 
 
 }
 
-class Chat extends Thread{
+class Connect extends AsyncTask<String, Void, Object> {
 
-    public String nick;
+    private static Socket clientSocket; //сокет для общения
+    private static BufferedReader reader; // нам нужен ридер читающий с консоли, иначе как
+    // мы узнаем что хочет сказать клиент?
+    private static BufferedReader in; // поток чтения из сокета
+    private static BufferedWriter out; // поток записи в сокет
 
     @Override
-    public void run() {
+    protected String doInBackground(String... strs) {
 
-        System.out.println("Инициализация подключения к серверу");
-
-        try (Socket socket = new Socket("localhost", 10001);
-             BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-             BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()))) {
-
-            System.out.println("Соединение установлено");
-
-//            info.setText("Соединение установлено");
-
-            PrintWriter pw = new PrintWriter(bw, true);
-            pw.println(nick);
-            String[] messages = {"Message1", "Message2", "exit"};//сообщения от клиента, последнее -команда выхода/завершенимя сеанса
-            AtomicInteger i = new AtomicInteger(0);
-            while (true) {
-                String clientMessage = messages[i.get()];
-                pw.println(clientMessage);
-                String messageServer = "";
-                String st = " ";
-                while ((st != null) && br.ready()) {
-                    st = br.readLine();
-                    messageServer += st + "\n";
-                }
-                System.out.println(messageServer);//ответ сервера
-                i.getAndIncrement();
-                if (i.intValue() > 2) break;
-                Thread.sleep(50);
+        try {
+            // адрес - локальный хост, порт - 4004, такой же как у сервера
+            Socket clientSocket = new Socket("192.168.0.239", 4004);
+            //  у сервера доступ на соединение
+            DataOutputStream dout = new DataOutputStream(clientSocket.getOutputStream());
+            for (String string : strs) {
+                dout.writeUTF(string);
             }
-        } catch (UnknownHostException | FileNotFoundException e) {
-            System.err.println(e.getMessage());
-        } catch (IOException e) {
-            System.err.println(e.getMessage());
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        catch (NetworkOnMainThreadException e){
-            e.printStackTrace();
-        }
-        finally {
-            System.out.println("Соединение разорвано");
-        }
-    }
 
+            dout.flush();
+            dout.close();
+            clientSocket.close();
+
+
+//            reader = new BufferedReader(new InputStreamReader(System.in));
+            // читать соообщения с сервера
+//            in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            // писать туда же
+//            out = new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream()));
+
+            System.out.println("Вы что-то хотели сказать? Введите это здесь:");
+            // если соединение произошло и потоки успешно созданы - мы можем
+            //  работать дальше и предложить клиенту что то ввести
+            // если нет - вылетит исключение
+//            String word = reader.readLine(); // ждём пока клиент что-нибудь
+            // не напишет в консоль
+//            out.write(word + "\n"); // отправляем сообщение на сервер
+//            out.flush();
+//            String serverWord = in.readLine(); // ждём, что скажет сервер
+//            System.out.println(serverWord); // получив - выводим на экран
+        } catch (Exception e) {
+            System.out.println("Увы, с сетью проблема"); //TODO тут что-то происходит, но что - неясно.
+        }
+        return null;
+
+    }
 }
