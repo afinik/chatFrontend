0a94cd23f32019d25ec6c07e36ddfaeea8dd7687
smalex <alexdate@gmail.com>
2020-01-29 18:16:17 +0300

    Сделано подключение к сокету

--- app/src/main/java/com/example/testchat/MainActivity.java
+++ app/src/main/java/com/example/testchat/MainActivity.java
@@ -6,6 +6,10 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.net.Socket;
 
 public class MainActivity extends Activity {
@@ -18,15 +22,18 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         TextView info = (TextView) findViewById(R.id.info1);
-        new Thread(() -> {connection();}).start();new Thread(() -> {connection();}).start();
+
+        new Thread(() -> {connection();}).start();
         info.setText("Запускаем процесс");
 
 
     }
 
     private void connection() {
-        try {
-            Socket socket = new Socket("192.168.1.8", 4004);
+        try(Socket socket = new Socket("192.168.1.8", 4004)){
+            BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            // BufferedReader fileReader = new BufferedReader(new FileReader("./httpPackage"));
+            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
         }
         catch (Exception e){
             System.out.println(e.getMessage());
